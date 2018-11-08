39705bef6abbd20b273a00c3e291ba39902ea98b
Christopher Brasington <chrisbrasington@gmail.com>
2018-11-08 12:11:43 -0700

    serial write

--- hello_hackathon/hello_hackathon.ino
+++ hello_hackathon/hello_hackathon.ino
@@ -7,18 +7,8 @@
 #define B   A1
 #define C   A2
 
-// Last parameter = 'true' enables double-buffering, for flicker-free,
-// buttery smooth animation.  Note that NOTHING WILL SHOW ON THE DISPLAY
-// until the first call to swapBuffers().  This is normal.
-RGBmatrixPanel matrix(A, B, C, CLK, LAT, OE, true);
-// Double-buffered mode consumes nearly all the RAM available on the
-// Arduino Uno -- only a handful of free bytes remain.  Even the
-// following string needs to go in PROGMEM:
+RGBmatrixPanel matrix(A, B, C, CLK, LAT, OE, false);
 
-// Similar to F(), but for PROGMEM string pointers rather than literals
-#define F2(progmem_ptr) (const __FlashStringHelper *)progmem_ptr
-
-//char const str[] PROGMEM = "HACK-A-THON";
 int16_t    textX         = matrix.width(),
            textMin       = 60 * -12,
            hue           = 0;
@@ -27,34 +17,82 @@ void setup() {
   matrix.begin();
   matrix.setTextWrap(false); // Allow text to run off right edge
   matrix.setTextSize(2);
+
+  Serial.begin(115200);
+  Serial.println("DONE");
+  
+}
+
+int incomingByte = 0; 
+int i = 0;
+char str[] = "                                        ";
+
+void resetBuffer() {
+   for(int x = 0; x < 40; x++ ) {
+      str[x] = ' ';
+   }
 }
 
 void loop() {
-  char str[] = "HACK-A-THON";
-  print(str, sizeof(str) * -12);
-  delay(10);
+  
+  //print(str, sizeof(str) * -12);
+  //delay(10);
+
+  // send data only when you receive data:
+  if (Serial.available() > 0) {
+    // read the incoming byte:
+    incomingByte = Serial.read();
+
+    // say what you got:
+    Serial.print("I received: ");
+    char c = incomingByte;
+    Serial.println(c);
+
+    str[i] = c;
+
+    i = i+1;
+    
+  }
+  else {
+    if(i > 0) {
+      // fill the screen with black
+      matrix.fillScreen(matrix.Color333(0, 0, 0));
+      
+      Serial.print("RESULT: ");
+      Serial.println(str);
+      Serial.println(i);
+      
+      print(str, i * -12);
+      delay(100);
+      
+      resetBuffer();
+    }
+    i = 0;
+  }
 }
+
 
 
 void print(char str[], int size) {
   textMin       = size;
   byte i;
 
-  // Clear background
-  matrix.fillScreen(0);
-  
-  // Draw big scrolly text on top
-  matrix.setTextColor(matrix.ColorHSV(hue, 255, 255, true));
-  matrix.setCursor(textX, 1);
-  //matrix.print(F2(str));
-  matrix.print(str);
+  Serial.println("PRINT!!!!");
+  Serial.println(str);
 
-  // Move text left (w/wrap), increase hue
-  if((--textX) < textMin) textX = matrix.width();
-  hue += 7;
-  if(hue >= 1536) hue -= 1536;
+  matrix.setCursor(2, 0);  // start at top left, with one pixel of spacing
+  matrix.setTextSize(1);   // size 1 == 8 pixels high
 
-  // Update display
-  matrix.swapBuffers(false);
+  // print each letter with a rainbow color
+  //matrix.print("12345");
+  matrix.setTextColor(matrix.Color333(7,0,0));
+  for(int x = 0; x <=5; x++){
+    matrix.print(str[x]);
+  }
 
+  matrix.setCursor(2, 9);  // next line
+  matrix.setTextColor(matrix.Color333(0,7,7));
+  for(int x = 5; x <= 10; x++){
+    matrix.print(str[x]);
+  }
 }
