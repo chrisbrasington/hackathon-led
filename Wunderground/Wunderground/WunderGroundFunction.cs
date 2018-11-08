d7a9b1a41a8cce7ba9ea260fe0851f70a595696f
Matt Mihoover <mattmihoover@gmail.com>
2018-11-08 13:15:37 -0700

    Real WunderGround fix

--- Wunderground/Wunderground/WunderGroundFunction.cs
+++ Wunderground/Wunderground/WunderGroundFunction.cs
@@ -3,6 +3,7 @@ using System.Net.Http;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Host;
 using Newtonsoft.Json;
+using System.Configuration;
 
 namespace Wunderground
 {
@@ -14,14 +15,13 @@ namespace Wunderground
          log.Info($"C# Timer trigger function executed at: {DateTime.Now}");
          using (var client = new HttpClient())
          {
-            client.BaseAddress = new Uri("http://api.wunderground.com/api/faded1290e956eb4/conditions/q/80111.json");
+            string apiKey = ConfigurationManager.AppSettings["WundergroundKey"];
+            client.BaseAddress = new Uri($"http://api.wunderground.com/api/{apiKey}/conditions/q/80111.json");
             var result = await client.GetAsync("");
             string resultContent = await result.Content.ReadAsStringAsync();
             RootObject a = JsonConvert.DeserializeObject<RootObject>(resultContent);
             string text = $"{a.current_observation.temperature_string} {a.current_observation.weather}";
             log.Info(text);
-            client.BaseAddress = new Uri("Chris' computer");
-            client.PostAsync("Chris' computer", new StringContent(text));
          }
 
       }
