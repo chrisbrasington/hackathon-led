efe1cfbcc34d9f91b0d1664ed9c01b5845738f04
Matt Mihoover <mattmihoover@gmail.com>
2018-11-08 14:01:05 -0700

    adding api to wunderground

--- Wunderground/Wunderground/WunderGroundFunction.cs
+++ Wunderground/Wunderground/WunderGroundFunction.cs
@@ -13,15 +13,17 @@ namespace Wunderground
       public static async void Run([TimerTrigger("0 */5 * * * *")]TimerInfo myTimer, TraceWriter log)
       {
          log.Info($"C# Timer trigger function executed at: {DateTime.Now}");
+         string apiKey = ConfigurationManager.AppSettings["WundergroundKey"];
+         string apiUrl = ConfigurationManager.AppSettings["ApiUrl"];
          using (var client = new HttpClient())
          {
-            string apiKey = ConfigurationManager.AppSettings["WundergroundKey"];
             client.BaseAddress = new Uri($"http://api.wunderground.com/api/{apiKey}/conditions/q/80111.json");
             var result = await client.GetAsync("");
             string resultContent = await result.Content.ReadAsStringAsync();
             RootObject a = JsonConvert.DeserializeObject<RootObject>(resultContent);
             string text = $"{a.current_observation.temperature_string} {a.current_observation.weather}";
             log.Info(text);
+            await client.PostAsync($"{apiUrl}={text}",null);
          }
 
       }
